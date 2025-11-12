f7feded0324801949b8e4e2951eb9e1c703d9419
_or_75 <or.75@ya.ru>
2025-11-12 21:56:03 +0300

    add manual map injector

--- Andromeda-Dota2/Andromeda/Common/Include/Config.hpp
+++ Andromeda-Dota2/Andromeda/Common/Include/Config.hpp
@@ -12,11 +12,11 @@
 
 #ifdef RELEASE_BUILD
 
-#define ENABLE_CONSOLE_DEBUG		1
-#define ENABLE_MANUAL_MAP			0
+#define ENABLE_CONSOLE_DEBUG		0
+#define ENABLE_MANUAL_MAP			1
 #define ENABLE_CPP_EH_EXCEPTION		0
 
-#define ENABLE_XOR_STR				0
+#define ENABLE_XOR_STR				1
 
 #define LOG_SDK_PATTERN				1
 
